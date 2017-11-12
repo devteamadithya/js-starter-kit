1b008a9c70096164dde6ab75426204a4139e48e7
jadithya5 <adithyajui@gmail.com>
2017-11-12 01:28:15 -0600

    automation pre message
    by using chalk npm we can prestart/poststart the file and give the message.check package.json for changes.

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,11 @@
+var chalk = require('chalk');
+
+console.log(chalk.blue('Your application is starting in dev mode'));
+
+//Chalk is used to give the message before/after running the application.
+//in package.json in the scripts prestart means before start application run the file.
+//poststart means after application loads give the message. start means run the file.
+//npm start it will dispay the message in the console.
+
+
+
