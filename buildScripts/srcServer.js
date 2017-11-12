1a77432cd7eed56e4a9411f003c60c0e4a8aecdd
jadithya5 <adithyajui@gmail.com>
2017-11-12 00:47:37 -0600

    shared url to team
    local tunnel is easy way and custom url server.
    Dev servers: express and browsersync are handy.
    Share work to team then localtunnel,now,surge is handy. npm install.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -27,5 +27,10 @@ app.listen(port,function(err){
 });
 
 //to run the express in terminal type ==> node buildScripts/srcServer.js
+// if you want to share your url with another to see your work local tunnel or now or surge is handy,browsersync.
+//loacaltunnel is easy you can create custom url to share.
+//to install type npm install localtunnel -g
+//to run the server type lt --port #number it returns the url. just copy and paste in browser.
+// custom url ==> lt --port #num adithya it will return custom url.
 
 
