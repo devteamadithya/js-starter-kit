6a2c703e27c7dde05752c1c656be1ec2b51d0a5f
jadithya5 <adithyajui@gmail.com>
2017-11-13 23:00:46 -0600

    JSDOM testing
    testing window DOM whether that is available or not. fs contains filessystem. DONE() can be used for asynchrounous testing.
    to run test type npm test or npm t

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,9 @@
 //importing chai for test
 import { expect } from 'chai';
+//DOM testing for that we need jsDOM and fs means filesystem to travel our folder.
+import jsdom from 'jsdom';
+import fs from 'fs';
+
 //describe gives the title of task
 describe('Our first test', () => {
   it('should pass', () => {
@@ -7,3 +11,17 @@ describe('Our first test', () => {
     expect(true).to.equal(true);
   })
 })
+
+//finding express running fine! is available or not in the window(browser).
+describe('index.html',()=>{
+  it('should have express',(done)=>{
+    const file = fs.readFileSync('./src/index.html',"utf-8");
+    jsdom.env(file,function(err,window){
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal("Express running fine!");
+      done();
+      window.close();
+    });
+  })
+})
+
